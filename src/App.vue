c59c147f0fc23acd7f60be82ae3541edc9385234
Sohee Lee <sohee.lee7@nhnent.com>
2018-09-27 18:17:37 +0900

    Initialize commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <div>
+            <h3>Props Test Buttons</h3>
+            <button @click="changeProp">changeProp</button>
+        </div>
+        <div>
+            <h3>Function Test Buttons</h3>
+            <button v-for="method in methodNames" :key="method" @click="methodInvoke(method)">
+                {{ method }}
+            </button>
+            <p>Function Result : {{ message }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            message: '',
+            methodNames: [
+                'method'
+            ]
+        };
+    },
+    methods: {
+        changeProp() {
+            // change prop
+        },
+        methodInvoke(methodName) {
+            // this.message = this.$refs.{something}.invoke(methodName);
+        }
+    }
+};
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.png$/,
+                loader: ['url-loader']
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        host: '0.0.0.0'
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
